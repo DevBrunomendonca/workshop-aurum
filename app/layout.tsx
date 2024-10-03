acfd400cf16344c0b874c48f7fff35c560fcc075
Bruno Mendonça <101358061+DevBrunomendonca@users.noreply.github.com>
2024-10-03 09:38:10 -0300

    feat: add component Footer to Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./_components/header";
 import localFont from "next/font/local"
+import Footer from "./_components/footer";
 
 export const modaniRegular = localFont({
   src: "./fonts/MadaniArabicRegular.ttf",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
