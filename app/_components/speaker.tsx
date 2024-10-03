31ba2f40d4fa708c46fa6d3ffd20d7c56858a173
Bruno Mendonça <101358061+DevBrunomendonca@users.noreply.github.com>
2024-10-03 19:50:11 -0300

    fix: text dataCard

--- app/_components/speaker.tsx
+++ app/_components/speaker.tsx
@@ -20,28 +20,28 @@ const Speaker = () => {
         <div className="grid items-start justify-center place-items-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-5">
             <CardSpeaker
             srcProfile="/profile-1.svg"
-            dataCard="Sex 19/10"
+            dataCard="Sáb 19/10"
             timeCard="8:40 às 9:00"
             nameProfile="ADRIANO CAMARGO"
             description="Abertura do workshop"
           />
           <CardSpeaker
             srcProfile="/profile-2.svg"
-            dataCard="Sex 19/10"
+            dataCard="Sáb 19/10"
             timeCard="9:00 às 9:30"
             nameProfile="WAGNER LUIZ"
             description="Oportunidades: Empreendendo no Mercado Financeiro."
           />
           <CardSpeaker
             srcProfile="/profile-3.svg"
-            dataCard="Sex 19/10"
+            dataCard="Sáb 19/10"
             timeCard="9:30 às 10:00"
             nameProfile="THIAGO EVANGELISTA"
             description="Primeiros Passos: Sobrevivência ao Crescimento."
           />
           <CardSpeaker
             srcProfile="/profile-4.svg"
-            dataCard="Sex 19/10"
+            dataCard="Sáb 19/10"
             timeCard="10:00 às 10:30"
             timeCardTwo="11:00 às 11:20"
             nameProfile="CAIO HENRIQUE"
@@ -49,7 +49,7 @@ const Speaker = () => {
           />
           <CardSpeaker
             srcProfile="/profile-5.svg"
-            dataCard="Sex 19/10"
+            dataCard="Sáb 19/10"
             timeCard="11:20 às 11:45"
             nameProfile="ISMAEL GUIMARÃES"
             description="Gerenciamento de Risco: o Segredo dos 5% que vencem."
