dc73e8912da4213b6d17d01ea9f65a4ffdf0111e
Bruno Mendonça <101358061+DevBrunomendonca@users.noreply.github.com>
2024-10-03 09:37:18 -0300

    feat: add component Footer

---
+++ app/_components/footer.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image"
+import { modaniBold } from "../layout"
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="bg-[#F3E902]">
+        <div className="max-w-6xl mx-auto px-4 justify-center flex flex-col items-center  md:flex-row">
+          <div className="flex w-full flex-col items-start gap-2">
+            <h2 className={`${modaniBold.className} text-4xl text-black`}>Dê o Próximo Passo Rumo ao Sucesso</h2>
+            <p className="text-sm text-black">Inscreva-se para nosso evento presencial e faça parte dos 5%</p>
+            <a
+              href="https://checkout.doppus.app/62522663/"
+              target="_blank"
+              className="rounded-md bg-black px-16 py-4 text-[#F3E902]"
+            >
+              Quero credenciar
+            </a>
+          </div>
+          <Image 
+          src="/map-pin.svg"
+          alt="Icone map pin"
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="w-full h-[450px]" 
+
+        />
+        </div>
+        <div className="bg-[#242424]">
+          <p className="text-white text-center text-sm p-4">© Copyright 2024 Aurum Capital - Developed by Dlack Tecnologia</p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+
+export default Footer
